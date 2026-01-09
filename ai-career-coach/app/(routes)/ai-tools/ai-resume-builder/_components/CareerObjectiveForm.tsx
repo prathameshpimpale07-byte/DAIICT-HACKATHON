4b9a45fbf6367cd867825720b099c07540a468c4
PRATHAMESH PIMPALE <prathameshpimpale07@gmail.com>
2026-01-09 21:19:59 +0530

    add Career Objective form component

---
+++ ai-career-coach/app/(routes)/ai-tools/ai-resume-builder/_components/CareerObjectiveForm.tsx
@@ -0,0 +1,15 @@
+"use client";
+import { Dispatch, SetStateAction } from "react";
+
+interface Props {
+    data: string;
+    setData: Dispatch<SetStateAction<string>>;
+}
+
+export default function CareerObjectiveForm({ data, setData }: Props) {
+    return (
+        <div>
+            
+        </div>
+    )
+}
