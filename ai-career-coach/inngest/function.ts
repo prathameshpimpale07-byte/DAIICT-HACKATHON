1812ca22c530b842d59af0b3832602535ac70726
Jeevan-2275 <jeevankadam2275@gmail.com>
2026-01-10 02:27:06 +0530

    chore(inngest): add hello world function and base setup

---
+++ ai-career-coach/inngest/function.ts
@@ -0,0 +1,13 @@
+import { db } from "@/configs/db";
+import { inngest } from "./client";
+
+export const helloWorld = inngest.createFunction(
+  { id: "hello-world" },
+  { event: "test/hello.world" },
+  async ({ event, step }) => {
+    await step.sleep("wait-a-moment", "1s");
+    return { message: `Hello ${event.data.email}!` };
+  },
+);
+
+const today = new Date().toDateString();
