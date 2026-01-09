aa2929d3906face94ef3ad82bc0d55cfe418923d
Nageshjagtap <nageshjagtap063@gmail.com>
2026-01-09 21:44:44 +0530

    create TurboNode custom node component

---
+++ ai-career-coach/app/(routes)/ai-tools/ai-roadmap-agent/_components/TurboNode.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Link from 'next/link'
+import { Hanalei } from 'next/font/google'
+import { Handle, Position } from '@xyflow/react';
+
+function ({data}: any){
+    return (
+        <div>
+
+        </div>
+    )
+}
+export default TurboNode;
